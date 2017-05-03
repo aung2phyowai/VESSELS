0cea5d7e2baeff156d9030b7d3ace951e824ffd3
nwayt001 <nick.waytowich@gmail.com>
2017-05-03 19:31:31 -0400

    fixed bug when trying to find the versin in windows 7 sp1. changed audio files to sound effects

--- VESSELS/core/Program.cs
+++ VESSELS/core/Program.cs
@@ -22,11 +22,20 @@ namespace VESSELS
             string os_version = Environment.OSVersion.ToString();
             char delimiter = ' ';
             string[] os_version_split = os_version.Split(delimiter);
-            string version_str = os_version_split[os_version_split.Length - 1];
-            Version version = Version.Parse(version_str);
-            // Windows 10 = 6.2, windows 7 = 6.1 
+            string version_str;
+            Version version = new Version();
+            for (int i=0; i<os_version_split.Length; i++)
+            {
+                try
+                {
+                    version_str = os_version_split[i];
+                    version = Version.Parse(version_str);
+                }
+                catch (Exception e) { }
+            }
             Globals.OS_VERSION = version;
-            
+            Console.WriteLine(version.Minor.ToString());
+
             // start main application
             using (Game1 game = new Game1())
             {
